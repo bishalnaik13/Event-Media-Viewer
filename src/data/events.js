6d25f6982b6551208d58ca6f75ed1986780cccf1
Bishal Naik <bishalbijaynaikdob2004@gmail.com>
2026-01-27 13:49:36 +0530

    feat: add sample event data

---
+++ src/data/events.js
@@ -0,0 +1,12 @@
+export const events = [
+  {
+    id: '1',
+    name: 'Tech Conference 2025',
+    code: 'TC25',
+  },
+  {
+    id: '2',
+    name: 'Wedding Ceremony',
+    code: 'WD09',
+  },
+];
